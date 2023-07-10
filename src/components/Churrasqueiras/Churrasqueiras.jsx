feadb93a07002c8a758895234466e7c8218b329d
franklpjr <eng.civ.frank@gmail.com>
2023-07-10 16:38:24 -0400

    Adicao de botao do whatsapp

--- src/components/Churrasqueiras/Churrasqueiras.jsx
+++ src/components/Churrasqueiras/Churrasqueiras.jsx
@@ -1,15 +1,24 @@
 import CardsCh from "../CardsCh/CardsCh";
 import GalleryCh from "../MultiCarouselCh/MultiCarouselCh";
 import "./Churrasqueiras.css"
+import whatsapp from "../../assets/whatsapp-icon.png"
 
 const Churrasqueiras = () => {
 
     return (
         <div id="churrasqueiras" className="churrasqueiras-wrapper">
-            <h1>Churrasqueiras <b>pré-moldadas</b></h1>
-            <p>Diversas opções de revestimento à sua escolha:</p>    
-            <GalleryCh />
-            <CardsCh />
+            <div>
+                <h1>Churrasqueiras <b>pré-moldadas</b></h1>
+                <p>Diversas opções de revestimento à sua escolha:</p>    
+                <GalleryCh />
+                <CardsCh />
+            </div>
+            <div className="botao-zap-wrapper">
+                <a href="https://wa.me/message/WRCA23A2C33DP1" class="botao-zap">
+                    Compre agora
+                    <img className="zap-icon" src={whatsapp} width="35px"/>
+                </a>
+            </div>
         </div>
     )
 
